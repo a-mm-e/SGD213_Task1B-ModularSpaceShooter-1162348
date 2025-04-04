b034d6fc0456a69d037cc8653e578cd0b0466f9d
a-mm-e <emmalusk42@gmail.com>
2025-04-04 10:15:48 +1000

    Import Inheritance Package
    Imported the Task1C Inheritance Package. There were slight issues with player movement with the player not moving. But after switching PlayerMovement scipts this was fixed. Also had to change the old player spaceship to the new Prefab spaceship.

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,32 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// PlayerInput handles all of the player specific input behaviour, and passes the input information
+/// to the appropriate scripts.
+/// </summary>
 public class PlayerInput : MonoBehaviour
 {
-    private PlayerMovementScript playerMovementScript;
-    private ShootingScript shootingScript;
 
-    // Initialise references. 
-    void Start()
+    // local references
+    private PlayerMovement playerMovement;
+
+    private WeaponBase weapon;
+    public WeaponBase Weapon
     {
-        playerMovementScript = GetComponent<PlayerMovementScript>();
-        shootingScript = GetComponent<ShootingScript>();
+        get
+        {
+            return weapon;
+        }
+
+        set
+        {
+            weapon = value;
+        }
     }
 
-    // Handle player input for movement and shooting. 
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        weapon = GetComponent<WeaponBase>();
+    }
+
     void Update()
     {
+        // read our horizontal input axis
         float horizontalInput = Input.GetAxis("Horizontal");
-
-        // Process horizontal movement input.
+        // if movement input is not zero
         if (horizontalInput != 0.0f)
         {
-            playerMovementScript.HorizontalMovement(horizontalInput);
+            // ensure our playerMovementScript is populated to avoid errors
+            if (playerMovement != null)
+            {
+                // pass our movement input to our playerMovementScript
+                playerMovement.MovePlayer(horizontalInput * Vector2.right);
+            }
         }
 
-        // Check if the player is pressing the fire button. 
+        // if we press the Fire1 button
         if (Input.GetButton("Fire1"))
         {
-            shootingScript.Shoot();
+            // if our shootingScript is populated
+            if (weapon != null)
+            {
+                // tell shootingScript to shoot
+                weapon.Shoot();
+            }
         }
+    }
+
+    /// <summary>
+    /// SwapWeapon handles creating a new WeaponBase component based on the given weaponType. This
+    /// will popluate the newWeapon's controls and remove the existing weapon ready for usage.
+    /// </summary>
+    /// <param name="weaponType">The given weaponType to swap our current weapon to, this is an enum in WeaponBase.cs</param>
+    public void SwapWeapon(WeaponType weaponType)
+    {
+        // make a new weapon dependent on the weaponType
+        WeaponBase newWeapon = null;
+        switch (weaponType)
+        {
+            case WeaponType.machineGun:
+                newWeapon = gameObject.AddComponent<WeaponMachineGun>();
+                break;
+            case WeaponType.tripleShot:
+                newWeapon = gameObject.AddComponent<WeaponTripleShot>();
+                break;
+        }
+
+        // update the data of our newWeapon with that of our current weapon
+        newWeapon.UpdateWeaponControls(weapon);
+        // remove the old weapon
+        Destroy(weapon);
+        // set our current weapon to be the newWeapon
+        weapon = newWeapon;
     }
 }
